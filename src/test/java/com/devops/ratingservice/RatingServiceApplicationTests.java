6cf2d2d3517ba457ca74f411e84949275948457f
isidora-stanic <isidora.stanic@outlook.com>
2023-08-16 11:25:19 +0200

    feat: added mongodb which runs as a container

--- src/test/java/com/devops/ratingservice/RatingServiceApplicationTests.java
+++ src/test/java/com/devops/ratingservice/RatingServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.devops.ratingservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestRatingServiceApplication.class)
 class RatingServiceApplicationTests {
 
 	@Test
